cd925062c434e02c854a6999c00dd9e202e7492b
Andrew Raub <raublekick@gmail.com>
2020-06-17 16:27:45 -0700

    Adding email

--- src/views/About.vue
+++ src/views/About.vue
@@ -8,24 +8,31 @@
         <h2 class="subtitle">
           A community-sourced project.
         </h2>
-        <p>
+        <p class="mt-4">
           The purpose of this project is to collect information about real
           customer experiences at Phoenix businesses during the COVID-19
           pandemic. This data can be used as a decision-making tool based on
           personal comfort level.
         </p>
-        <p>
+        <p class="mt-4">
           <strong>Disclaimer:</strong> The responses displayed on this site are
           community-sourced and do not reflect the views of the site owners.
         </p>
 
-        <p>
+        <p class="mt-4">
           This project is licensed under the MIT open source license.
           <a
             href="https://github.com/raublekick/phx-covid-businesses"
             target="_blank"
             >View on GitHub</a
           >
+        </p>
+
+        <p class="mt-4">
+          For questions about this project, please contact:
+          <a href="mailto:businessesandcovid@gmail.com "
+            >businessesandcovid@gmail.com
+          </a>
         </p>
       </div>
     </div>
